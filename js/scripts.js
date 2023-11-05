2ced938466e4048c1e9b1b53f1cd6b62f00bfd40
Jacek Nogaj <jacek.nogaj@gmail.com>
2023-11-05 23:44:02 +0100

    project corrected

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ const incomeList = document.getElementById("incomeList");
 const totalExpenseText = document.getElementById("totalExpense");
 const totalIncomeText = document.getElementById("totalIncome");
 const modal = document.getElementById("modal");
-const modalContent = document.getElementById("modalContent");
 const editForm = document.getElementById("editForm");
 const editName = document.getElementById("editName");
 const editAmount = document.getElementById("editAmount");
@@ -16,8 +15,8 @@ const balanceMsg = document.getElementById("balanceMsg");
 
 const incomeData = [];
 const expenseData = [];
-const editingIndex = -1;
-const editingType = "";
+let editingIndex = -1;
+let editingType = "";
 
 incomeForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -25,15 +24,15 @@ incomeForm.addEventListener("submit", (e) => {
   const name = document.getElementById("incomeName").value;
   const amount = parseFloat(document.getElementById("incomeAmount").value);
 
-  if (name && !isNaN(amount)) {
-    incomeData.push({ name, amount });
+  if (name && !isNaN(amount) && amount > 0) {
+    incomeData.push({ name, amount: amount.toFixed(2) });
     updateBudgetList();
     document.getElementById("incomeName").value = "";
     document.getElementById("incomeAmount").value = "";
     calculateBalance();
-    errorMsg.textContent = "";
   } else {
-    errorMsg.textContent = "Wprowadź poprawne dane.";
+    errorMsg.textContent =
+      "Wprowadź poprawne dane (kwota musi być większa od 0).";
   }
 });
 
@@ -43,15 +42,15 @@ expenseForm.addEventListener("submit", (e) => {
   const name = document.getElementById("expenseName").value;
   const amount = parseFloat(document.getElementById("expenseAmount").value);
 
-  if (name && !isNaN(amount)) {
-    expenseData.push({ name, amount });
+  if (name && !isNaN(amount) && amount > 0) {
+    expenseData.push({ name, amount: amount.toFixed(2) });
     updateBudgetList();
     document.getElementById("expenseName").value = "";
     document.getElementById("expenseAmount").value = "";
     calculateBalance();
-    errorMsg.textContent = "";
   } else {
-    errorMsg.textContent = "Wprowadź poprawne dane.";
+    errorMsg.textContent =
+      "Wprowadź poprawne dane (kwota musi być większa od 0).";
   }
 });
 
@@ -59,23 +58,23 @@ editForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const newName = editName.value;
-  const newAmount = parseFloat(editAmount.value).toFixed(2);
+  const newAmount = parseFloat(editAmount.value);
 
-  if (newName && !isNaN(newAmount)) {
+  if (newName && !isNaN(newAmount) && newAmount > 0) {
     if (editingType === "income") {
       incomeData[editingIndex].name = newName;
-      incomeData[editingIndex].amount = newAmount;
-    } else {
+      incomeData[editingIndex].amount = newAmount.toFixed(2);
+    } else if (editingType === "expense") {
       expenseData[editingIndex].name = newName;
-      expenseData[editingIndex].amount = newAmount;
+      expenseData[editingIndex].amount = newAmount.toFixed(2);
     }
 
     updateBudgetList();
     calculateBalance();
 
     modal.style.display = "none";
-  } else {
-    errorMsg.textContent = "Wprowadź poprawne dane.";
+    document.getElementById("editName").value = "";
+    document.getElementById("editAmount").value = "";
   }
 });
 
@@ -85,80 +84,13 @@ function updateBudgetList() {
 
   for (let i = 0; i < incomeData.length; i++) {
     const item = incomeData[i];
-    const li = document.createElement("li");
-
-    const divTextList = document.createElement("div");
-    divTextList.className = "textList";
-
-    const divTextLeft = document.createElement("div");
-    divTextLeft.className = "textLeft";
-    divTextLeft.textContent = item.name + ":";
-
-    const divTextRight = document.createElement("div");
-    divTextRight.className = "textRight";
-    divTextRight.textContent = item.amount.toFixed(2) + " PLN";
-
-    divTextList.appendChild(divTextLeft);
-    divTextList.appendChild(divTextRight);
-    li.appendChild(divTextList);
-
-    const editBtn = document.createElement("button");
-
-    editBtn.className = "editBtn";
-    editBtn.textContent = "Edytuj";
-    editBtn.addEventListener("click", () => {
-      openEditModal(i, "income");
-    });
-
-    const deleteBtn = document.createElement("button");
-
-    deleteBtn.className = "deleteBtn";
-    deleteBtn.textContent = "Usuń";
-    deleteBtn.addEventListener("click", () => {
-      deleteBudgetItem(i, "income");
-    });
-    li.appendChild(editBtn);
-    li.appendChild(deleteBtn);
+    const li = createListItem(item, i, "income");
     incomeList.appendChild(li);
   }
 
   for (let i = 0; i < expenseData.length; i++) {
     const item = expenseData[i];
-    const li = document.createElement("li");
-
-    const divTextList = document.createElement("div");
-    divTextList.className = "textList";
-
-    const divTextLeft = document.createElement("div");
-    divTextLeft.className = "textLeft";
-    divTextLeft.textContent = item.name + ":";
-
-    const divTextRight = document.createElement("div");
-    divTextRight.className = "textRight";
-    divTextRight.textContent = item.amount.toFixed(2) + " PLN";
-
-    divTextList.appendChild(divTextLeft);
-    divTextList.appendChild(divTextRight);
-    li.appendChild(divTextList);
-
-    const editBtn = document.createElement("button");
-
-    editBtn.className = "editBtn";
-    editBtn.textContent = "Edytuj";
-    editBtn.addEventListener("click", () => {
-      openEditModal(i, "expense");
-    });
-
-    const deleteBtn = document.createElement("button");
-
-    deleteBtn.className = "deleteBtn";
-    deleteBtn.textContent = "Usuń";
-    deleteBtn.addEventListener("click", () => {
-      deleteBudgetItem(i, "expense");
-    });
-
-    li.appendChild(editBtn);
-    li.appendChild(deleteBtn);
+    const li = createListItem(item, i, "expense");
     expenseList.appendChild(li);
   }
 
@@ -166,38 +98,83 @@ function updateBudgetList() {
   calculateTotalExpense();
 }
 
+function createListItem(item, index, type) {
+  const li = document.createElement("li");
+
+  const divTextList = document.createElement("div");
+  divTextList.className = "textList";
+
+  const divTextLeft = document.createElement("div");
+  divTextLeft.className = "textLeft";
+  divTextLeft.textContent = item.name + ":";
+
+  const divTextRight = document.createElement("div");
+  divTextRight.className = "textRight";
+  divTextRight.textContent = item.amount + " PLN";
+
+  divTextList.appendChild(divTextLeft);
+  divTextList.appendChild(divTextRight);
+  li.appendChild(divTextList);
+
+  const editBtn = document.createElement("button");
+  editBtn.className = "btn editBtn";
+  editBtn.textContent = "Edytuj";
+  editBtn.addEventListener("click", () => {
+    openEditModal(index, type);
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.className = "btn deleteBtn";
+  deleteBtn.textContent = "Usuń";
+  deleteBtn.addEventListener("click", () => {
+    deleteBudgetItem(index, type);
+  });
+
+  li.appendChild(editBtn);
+  li.appendChild(deleteBtn);
+  return li;
+}
+
 function openEditModal(index, type) {
   const item = type === "expense" ? expenseData[index] : incomeData[index];
   editName.value = item.name;
-  editAmount.value = item.amount;
+  editAmount.value = parseFloat(item.amount);
+  editingIndex = index;
+  editingType = type;
   modal.style.display = "block";
-
-  editSaveBtn.onclick = () => {
-    const newName = editName.value;
-    const newAmount = parseFloat(editAmount.value);
-
-    if (newName && !isNaN(newAmount)) {
-      if (type === "expense") {
-        expenseData[index].name = newName;
-        expenseData[index].amount = newAmount;
-      } else {
-        incomeData[index].name = newName;
-        incomeData[index].amount = newAmount;
-      }
-
-      updateBudgetList();
-      calculateBalance();
-
-      modal.style.display = "none";
-    } else {
-      errorMsg.textContent = "Wprowadź poprawne dane.";
-    }
-  };
-
-  editCancelBtn.onclick = () => {
-    modal.style.display = "none";
-  };
 }
+
+function handleEditCancel() {
+  modal.style.display = "none";
+  document.getElementById("editName").value = "";
+  document.getElementById("editAmount").value = "";
+}
+
+editSaveBtn.addEventListener("click", () => {
+  const newName = editName.value;
+  const newAmount = parseFloat(editAmount.value);
+
+  if (newName && !isNaN(newAmount) && newAmount > 0) {
+    if (editingType === "income") {
+      incomeData[editingIndex].name = newName;
+      incomeData[editingIndex].amount = newAmount.toFixed(2);
+    } else if (editingType === "expense") {
+      expenseData[editingIndex].name = newName;
+      expenseData[editingIndex].amount = newAmount.toFixed(2);
+    }
+
+    updateBudgetList();
+    calculateBalance();
+
+    modal.style.display = "none";
+    document.getElementById("editName").value = "";
+    document.getElementById("editAmount").value = "";
+  } else {
+    errorMsg.textContent = "Wprowadź poprawne dane.";
+  }
+});
+
+editCancelBtn.addEventListener("click", handleEditCancel);
 
 function deleteBudgetItem(index, type) {
   if (type === "expense") {
@@ -212,34 +189,34 @@ function deleteBudgetItem(index, type) {
 
 function calculateBalance() {
   const totalIncome = incomeData
-    .reduce((sum, item) => sum + item.amount, 0)
+    .reduce((sum, item) => sum + parseFloat(item.amount), 0)
     .toFixed(2);
   const totalExpense = expenseData
-    .reduce((sum, item) => sum + item.amount, 0)
+    .reduce((sum, item) => sum + parseFloat(item.amount), 0)
     .toFixed(2);
   const balance = (totalIncome - totalExpense).toFixed(2);
 
   if (balance > 0) {
     balanceMsg.textContent = `Możesz jeszcze wydać ${balance} złotych.`;
-  } else if (balance === 0) {
-    balanceMsg.textContent = "Bilans wynosi zero.";
-  } else {
+  } else if (balance < 0) {
     balanceMsg.textContent = `Bilans jest ujemny. Jesteś na minusie ${Math.abs(
       balance
     )} złotych.`;
+  } else {
+    balanceMsg.textContent = "Bilans wynosi zero.";
   }
 }
 
 function calculateTotalExpense() {
   const totalExpense = expenseData
-    .reduce((sum, item) => sum + item.amount, 0)
+    .reduce((sum, item) => sum + parseFloat(item.amount), 0)
     .toFixed(2);
   totalExpenseText.textContent = `${totalExpense} PLN`;
 }
 
 function calculateTotalIncome() {
   const totalIncome = incomeData
-    .reduce((sum, item) => sum + item.amount, 0)
+    .reduce((sum, item) => sum + parseFloat(item.amount), 0)
     .toFixed(2);
   totalIncomeText.textContent = `${totalIncome} PLN`;
 }

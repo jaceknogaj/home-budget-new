e88873dced33fd4b249eb357ce584c3c579af046
Jacek Nogaj <jacek.nogaj@gmail.com>
2023-11-05 02:07:59 +0100

    project corrected

---
+++ js/scripts.js
@@ -0,0 +1,247 @@
+const incomeForm = document.getElementById("incomeForm");
+const expenseForm = document.getElementById("expenseForm");
+const expenseList = document.getElementById("expenseList");
+const incomeList = document.getElementById("incomeList");
+const totalExpenseText = document.getElementById("totalExpense");
+const totalIncomeText = document.getElementById("totalIncome");
+const modal = document.getElementById("modal");
+const modalContent = document.getElementById("modalContent");
+const editForm = document.getElementById("editForm");
+const editName = document.getElementById("editName");
+const editAmount = document.getElementById("editAmount");
+const editSaveBtn = document.getElementById("editSave");
+const editCancelBtn = document.getElementById("editCancel");
+const errorMsg = document.getElementById("errorMsg");
+const balanceMsg = document.getElementById("balanceMsg");
+
+const incomeData = [];
+const expenseData = [];
+const editingIndex = -1;
+const editingType = "";
+
+incomeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("incomeName").value;
+  const amount = parseFloat(document.getElementById("incomeAmount").value);
+
+  if (name && !isNaN(amount)) {
+    incomeData.push({ name, amount });
+    updateBudgetList();
+    document.getElementById("incomeName").value = "";
+    document.getElementById("incomeAmount").value = "";
+    calculateBalance();
+    errorMsg.textContent = "";
+  } else {
+    errorMsg.textContent = "Wprowadź poprawne dane.";
+  }
+});
+
+expenseForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("expenseName").value;
+  const amount = parseFloat(document.getElementById("expenseAmount").value);
+
+  if (name && !isNaN(amount)) {
+    expenseData.push({ name, amount });
+    updateBudgetList();
+    document.getElementById("expenseName").value = "";
+    document.getElementById("expenseAmount").value = "";
+    calculateBalance();
+    errorMsg.textContent = "";
+  } else {
+    errorMsg.textContent = "Wprowadź poprawne dane.";
+  }
+});
+
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const newName = editName.value;
+  const newAmount = parseFloat(editAmount.value).toFixed(2);
+
+  if (newName && !isNaN(newAmount)) {
+    if (editingType === "income") {
+      incomeData[editingIndex].name = newName;
+      incomeData[editingIndex].amount = newAmount;
+    } else {
+      expenseData[editingIndex].name = newName;
+      expenseData[editingIndex].amount = newAmount;
+    }
+
+    updateBudgetList();
+    calculateBalance();
+
+    modal.style.display = "none";
+  } else {
+    errorMsg.textContent = "Wprowadź poprawne dane.";
+  }
+});
+
+function updateBudgetList() {
+  expenseList.innerHTML = "";
+  incomeList.innerHTML = "";
+
+  for (let i = 0; i < incomeData.length; i++) {
+    const item = incomeData[i];
+    const li = document.createElement("li");
+
+    const divTextList = document.createElement("div");
+    divTextList.className = "textList";
+
+    const divTextLeft = document.createElement("div");
+    divTextLeft.className = "textLeft";
+    divTextLeft.textContent = item.name + ":";
+
+    const divTextRight = document.createElement("div");
+    divTextRight.className = "textRight";
+    divTextRight.textContent = item.amount.toFixed(2) + " PLN";
+
+    divTextList.appendChild(divTextLeft);
+    divTextList.appendChild(divTextRight);
+    li.appendChild(divTextList);
+
+    const editBtn = document.createElement("button");
+
+    editBtn.className = "editBtn";
+    editBtn.textContent = "Edytuj";
+    editBtn.addEventListener("click", () => {
+      openEditModal(i, "income");
+    });
+
+    const deleteBtn = document.createElement("button");
+
+    deleteBtn.className = "deleteBtn";
+    deleteBtn.textContent = "Usuń";
+    deleteBtn.addEventListener("click", () => {
+      deleteBudgetItem(i, "income");
+    });
+    li.appendChild(editBtn);
+    li.appendChild(deleteBtn);
+    incomeList.appendChild(li);
+  }
+
+  for (let i = 0; i < expenseData.length; i++) {
+    const item = expenseData[i];
+    const li = document.createElement("li");
+
+    const divTextList = document.createElement("div");
+    divTextList.className = "textList";
+
+    const divTextLeft = document.createElement("div");
+    divTextLeft.className = "textLeft";
+    divTextLeft.textContent = item.name + ":";
+
+    const divTextRight = document.createElement("div");
+    divTextRight.className = "textRight";
+    divTextRight.textContent = item.amount.toFixed(2) + " PLN";
+
+    divTextList.appendChild(divTextLeft);
+    divTextList.appendChild(divTextRight);
+    li.appendChild(divTextList);
+
+    const editBtn = document.createElement("button");
+
+    editBtn.className = "editBtn";
+    editBtn.textContent = "Edytuj";
+    editBtn.addEventListener("click", () => {
+      openEditModal(i, "expense");
+    });
+
+    const deleteBtn = document.createElement("button");
+
+    deleteBtn.className = "deleteBtn";
+    deleteBtn.textContent = "Usuń";
+    deleteBtn.addEventListener("click", () => {
+      deleteBudgetItem(i, "expense");
+    });
+
+    li.appendChild(editBtn);
+    li.appendChild(deleteBtn);
+    expenseList.appendChild(li);
+  }
+
+  calculateTotalIncome();
+  calculateTotalExpense();
+}
+
+function openEditModal(index, type) {
+  const item = type === "expense" ? expenseData[index] : incomeData[index];
+  editName.value = item.name;
+  editAmount.value = item.amount;
+  modal.style.display = "block";
+
+  editSaveBtn.onclick = () => {
+    const newName = editName.value;
+    const newAmount = parseFloat(editAmount.value);
+
+    if (newName && !isNaN(newAmount)) {
+      if (type === "expense") {
+        expenseData[index].name = newName;
+        expenseData[index].amount = newAmount;
+      } else {
+        incomeData[index].name = newName;
+        incomeData[index].amount = newAmount;
+      }
+
+      updateBudgetList();
+      calculateBalance();
+
+      modal.style.display = "none";
+    } else {
+      errorMsg.textContent = "Wprowadź poprawne dane.";
+    }
+  };
+
+  editCancelBtn.onclick = () => {
+    modal.style.display = "none";
+  };
+}
+
+function deleteBudgetItem(index, type) {
+  if (type === "expense") {
+    expenseData.splice(index, 1);
+  } else {
+    incomeData.splice(index, 1);
+  }
+
+  updateBudgetList();
+  calculateBalance();
+}
+
+function calculateBalance() {
+  const totalIncome = incomeData
+    .reduce((sum, item) => sum + item.amount, 0)
+    .toFixed(2);
+  const totalExpense = expenseData
+    .reduce((sum, item) => sum + item.amount, 0)
+    .toFixed(2);
+  const balance = (totalIncome - totalExpense).toFixed(2);
+
+  if (balance > 0) {
+    balanceMsg.textContent = `Możesz jeszcze wydać ${balance} złotych.`;
+  } else if (balance === 0) {
+    balanceMsg.textContent = "Bilans wynosi zero.";
+  } else {
+    balanceMsg.textContent = `Bilans jest ujemny. Jesteś na minusie ${Math.abs(
+      balance
+    )} złotych.`;
+  }
+}
+
+function calculateTotalExpense() {
+  const totalExpense = expenseData
+    .reduce((sum, item) => sum + item.amount, 0)
+    .toFixed(2);
+  totalExpenseText.textContent = `${totalExpense} PLN`;
+}
+
+function calculateTotalIncome() {
+  const totalIncome = incomeData
+    .reduce((sum, item) => sum + item.amount, 0)
+    .toFixed(2);
+  totalIncomeText.textContent = `${totalIncome} PLN`;
+}
+
+calculateBalance();
